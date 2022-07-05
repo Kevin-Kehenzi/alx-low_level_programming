e4c8076a44ba990b966aa61771ed80ed7d73ca8d
Kevin Kehenzi <kevinmwole6@gmail.com>
2022-07-05 12:05:50 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * _is alpha - check for alphabetic character
+ *_isalpha - checks character type
+ * @c: character to check
+ *
+ * Description: checks for alpha
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
